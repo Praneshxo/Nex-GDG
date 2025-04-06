89804ccabcdc4f8705ade987132a7d05ea814e44
Praneshxo <praneshgara@gmail.com>
2025-04-06 18:42:14 +0530

    april 6 all page connected

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Carrierguide from "./Carrierguide";
 import Home from "./Home.jsx";
 import Pdfpage from "./Pdfpage";
+import PdfLibrary from "./PdfLibrary";
+import ContinueLearning from "./ContinueLearning";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/Pdfpage" element={<Pdfpage />} />
         <Route path="/Carrierguide" element={<Carrierguide />} />
+        <Route path="/Pdflibrary" element={<PdfLibrary />} />
+        <Route path="/ContinueLearning" element={<ContinueLearning />} />
       </Routes>
     </Router>
   );
